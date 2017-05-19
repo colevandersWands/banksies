1ff56438b9434b95a330a365e5d9f632fe627da3
Evan Cole <master@Evans-MacBook-Pro.local>
2017-05-19 19:23:01 +0200

    messa things

---
+++ part1/simplestPassingFuncs/principles.js
@@ -0,0 +1,17 @@
+// In today's exercises, simplest passing functions will seem a little silly.  
+// This is to be expected.  
+// Today we are exploring the concept in isolation so next week we can incorporate it into the project.
+
+
+// The concept of a simplest passing function will become very important next week.
+// Next week's project is a multi-file javascript app to run in your browser.
+// The first step in developing the project will be to create a dummy project.
+// Your dummy project will consist of all the objects required by the prompt,
+//	 only every method will return hard-coded values.
+// The purpose of this is for you to make sure your program's structure is correct 
+// 	 before you spend loads of time trying to code the tricky bits.
+
+// Unit Tests
+// Testing and it's role in development is a whole nother discussion.
+// We will be looking at Test Driven Development in week 6.
+// This series of exercises is simply an introduction to what tests are.
---
+++ part1/simplestPassingFuncs/simplestFunctions.js
@@ -0,0 +1,12 @@
+// A simplest passing function for a test will have an acceptable answer hard coded
+
+// A simplest passing function for the test in unitTests.js looks like this:
+function addition(a, b) {
+	return 9;
+}
+
+
+// In this example simplest functions seem useless.
+// Moving forward, the 'correct answer' a test is expecting will be a return value of the right type.
+//  This might be an integer, an array, or even an object that satisfies your schema.
+
